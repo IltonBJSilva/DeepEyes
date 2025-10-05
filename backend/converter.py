f53ff684cf1479180299a1f829875679354ff871
chinesdecrocks <gooldelucas@gmail.com>
2025-10-05 14:22:14 -0300

    Adiciona data do dia

--- backend/converter.py
+++ backend/converter.py
@@ -1,13 +1,16 @@
 import requests
-from app import NASA_API_KEY
+from .app import NASA_API_KEY
 from PIL import Image
 from io import BytesIO
 
 # Coloque sua chave da NASA aqui
 api_key = "2lKrd3NQRCRAadHid5sSA7k0P0pZW5uwr4Lca7BU"
 
+date = input("Insert the date(YYYY-MM-DD): " )
+
+
 # Endpoint da APOD
-url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
+url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}&date={date}"
 
 # Faz a requisição
 res = requests.get(url)
